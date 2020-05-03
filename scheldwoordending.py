7a3670e0b762156d051cc7067b0148e61f4238ab
Greggy2645 <64741164+Greggy2645@users.noreply.github.com>
2020-05-03 20:23:22 +0200

    Update scheldwoordending.py

--- scheldwoordending.py
+++ scheldwoordending.py
@@ -1,26 +1,46 @@
+import time 
+#niet meer nodig, ooit gebruikt voor sleep ding
 scheldwoorden = ["fuck", "kut", "jk :))"]
+# dit was de array van scheldwoorden 
+#keuzeding functie begint hier
 def startding():
-    keuze = str(input('kies tussen a en b \n a is de array en b is toevoegen aan de array\n'))
+    keuze = str(input('kies tussen a en b \n a is de array en b is toevoegen aan de array\n en typ c voor verwijderen laatste scheldwoord'))
     if keuze == 'a':
         alles()
     elif keuze == 'b':
         toevoegen()
+    elif keuze == 'c':
+        verwijderen()	
+					
+		#alles anders dan a,b en c	
     else:
         print('jij idoot kan echt niks lmao')
         startding()
+#keuze ding eindigt hier
+#geeft de volledige array aan
 def alles():
     print(scheldwoorden)
     startding()
-
+#toevoegen in de scheldwoorden array, automatisch naar str
 def toevoegen():
-    scheldwoorden.append(str(input("geef scheldwoorden nu")))
+    scheldwoorden.append(str(input("geef scheldwoorden nu:\n")))
     startding()
+#functie van verwijderen scheldwoord, moet antwoord geven in nummer
+def verwijderen():	
+    scheldwoorden.pop(int(input('welke moet weg, kies nummer:\n')))
+    startding() 
+ 
 
-keuze = str(input('kies tussen a en b \n a is de array en b is toevoegen aan de array\n'))
+		
+		
+#eerste message voor keuze	
+keuze = str(input('kies tussen a en b \n a is de array en b is toevoegen aan de array\n en c + nummer is verwijderen'))
 if keuze == 'a':
     alles()
 elif keuze == 'b':
     toevoegen()
+elif keuze == 'c':
+    verwijderen()
 else:
     print('jij idoot kan echt niks lmao')
     startding()
