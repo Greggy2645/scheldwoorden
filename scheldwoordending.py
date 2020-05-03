029fb945e9f94252b1b0db177e689ede07cdbc42
Terebo <63092966+Terebo@users.noreply.github.com>
2020-05-03 18:25:57 +0200

    Terebosbugfix (#1)
    * hahah\hhahahahahaha
    
    noeb! heb ik even gefixt

--- scheldwoordending.py
+++ scheldwoordending.py
@@ -7,7 +7,7 @@ def startding():
         toevoegen()
     else:
         print('jij idoot kan echt niks lmao')
-startding()
+        startding()
 def alles():
     print(scheldwoorden)
     startding()
@@ -16,3 +16,11 @@ def toevoegen():
     scheldwoorden.append(str(input("geef scheldwoorden nu")))
     startding()
 
+keuze = str(input('kies tussen a en b \n a is de array en b is toevoegen aan de array\n'))
+if keuze == 'a':
+    alles()
+elif keuze == 'b':
+    toevoegen()
+else:
+    print('jij idoot kan echt niks lmao')
+    startding()
